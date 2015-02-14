016706311ece402e081d55fb2b9c043fc3195b07
Kurt Bartholomew <siever246@gmail.com>
2015-02-14 12:43:48 -0800

    Added index.html, jquery, stylesheet, and main client javascript file.

--- webserver/app.js
+++ webserver/app.js
@@ -5,7 +5,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var subsocket = require('./lib/subscribe');
+var subSocket = require('./lib/subscribe');
+var badges = require('./models/badges'); 
 
 server.listen(3000, function() {
 	console.log('Server is running on port %d', 3000);
@@ -20,8 +21,8 @@ app.get('/', function(req, res) {
 	res.send('./public/index.html');
 });
 
-io.socket.on('connection', function(socket) {
-	model.get(function(err, data) {
+io.sockets.on('connection', function(socket) {
+	badges.get(function(err, data) {
 		if (err) return;
 		data.forEach(function(badge) {
 			socket.emit('badge', badge);
@@ -30,5 +31,5 @@ io.socket.on('connection', function(socket) {
 });
 
 subSocket.on('message', function(message) {
-	io.socket.emit('badge', message);
+	io.sockets.emit('badge', message);
 });
